d37d03ea7aa4bf8deab593e51119dc7382a9f5b1
Bunma93 <bunma93@gmail.com>
2024-07-10 15:37:41 +0700

    Js-arr33

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // write code here
+const array = [
+    { name: 'apple', age: 14 },
+    { name: 'banana', age: 18 },
+    { name: 'watermelon', age: 32 }
+  ];
+
+let result = array.map(x => x.age);
+console.log(result);
